57361a77ac41b9ad375fe2089f41bb91249825be
AnnValle <annlya1120@gmail.com>
2026-04-15 17:30:45 -0600

    Refactor Index.php to improve code clarity

--- src/Controllers/Index.php
+++ src/Controllers/Index.php
@@ -1,36 +1,15 @@
 <?php
-/**
- * PHP Version 7.2
- *
- * @category Public
- * @package  Controllers
- * @author   Orlando J Betancourth <orlando.betancourth@gmail.com>
- * @license  MIT http://
- * @version  CVS:1.0.0
- * @link     http://
- */
+
 namespace Controllers;
 
-/**
- * Index Controller
- *
- * @category Public
- * @package  Controllers
- * @author   Orlando J Betancourth <orlando.betancourth@gmail.com>
- * @license  MIT http://
- * @link     http://
- */
+use Views\Renderer;
+
 class Index extends PublicController
 {
-    /**
-     * Index run method
-     *
-     * @return void
-     */
-    public function run() :void
+    public function run(): void
     {
         $viewData = array();
-        \Views\Renderer::render("index", $viewData);
+        Renderer::render("index", $viewData);
     }
 }
-?>
+
